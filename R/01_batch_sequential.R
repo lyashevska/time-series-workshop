64e46dd9f9a470f02d5bde2deb1104f9c5ad79a8
Coilin Minto <coilin.minto@gmit.ie>
2017-09-05 00:28:58 +0100

    Added batch and sequential processing code

--- R/01_batch_sequential.R
+++ R/01_batch_sequential.R
@@ -1,5 +1,5 @@
 ##-----------------------------
-## Sequential parametric tests live coding session
+## Batch and sequential parametric tests
 ## CM: 04/10/2017
 ## EPA tipping points workshop
 ##-----------------------------
@@ -8,13 +8,15 @@
 library(cpm)
 
 ## read in the data
-wind <- read.csv("../data/wind_speed.csv")
+wind <- read.csv("../data/wind-speed.csv")
 
 year <- wind$year
 y <- wind$speed
+n <- length(y)
+ylab <- "Speed (knots)"
 
 ## plot the data
-plot(year, y, type = "o", col = "purple", ylab = "Speed (knots)", pch = 19, bty = "l")
+plot(year, y, type = "o", col = "purple", ylab = ylab, pch = 19, bty = "l")
 
 ##---------------------------------------
 ## BATCH PROCESSING - SINGLE CHANGEPOINT 
@@ -37,7 +39,7 @@ abline(h = resultsMW$threshold, lty = 2)
 points(year[resultsMW$changePoint], resultsMW$Ds[resultsMW$changePoint], col = "red", cex = 2)
 
 ## plot the data
-plot(year, y, type = "o", col = "purple", ylab = "Speed (knots)", pch = 19, bty = "l", main = "Batch processing changepoints")
+plot(year, y, type = "o", col = "purple", ylab = ylab, pch = 19, bty = "l", main = "Batch processing changepoints")
 abline(v = year[resultsStudent$changePoint], lty = 2, col = "magenta" )
 abline(v = year[resultsMW$changePoint], lty = 2, col = "forestgreen")
 legend("topright", legend = c("Student t", "Mann-Whitney"), lty = 2, col = c("magenta", "forestgreen"), bty = "n")
@@ -48,22 +50,16 @@ legend("topright", legend = c("Student t", "Mann-Whitney"), lty = 2, col = c("ma
 ## Student t
 processStudent <- processStream(y, cpmType = "Student", startup = 10)
 ## 
-## plot out the test statistic
-plot(year, processStudent$Ds)
-abline(h = processStudent$threshold, lty = 2)
-points(year[processStudent$changePoint], processStudent$Ds[processStudent$changePoint], col = "red", cex = 2)
 
 k <- c(0, processStudent$changePoints, n)
-cols <- c("blue", "green2", "magenta")
+cols <- rainbow(length(processStudent$changePoints) + 1)
 
-pdf("../tex/figures/multiple_cps.pdf", height = 6, width = 8)
-plot.base()
+plot(year, y, type = "o", col = "darkgrey", ylab = ylab, pch = 19, bty = "l", main = "Sequential processing changepoints")
 for(i in 1:3){
     idx <- (k[i] + 1):k[i + 1]
-    points(time.plot[idx], y[idx], pch = 19, col = cols[i])
-    curve(mean(y[idx]) + 0 * x, from = time.plot[k[i] + 1], to = time.plot[k[i+1]], add = TRUE, col = cols[i])
+    points(year[idx], y[idx], pch = 19, col = cols[i])
+    curve(mean(y[idx]) + 0 * x, from = year[k[i] + 1], to = year[k[i+1]], add = TRUE, col = cols[i])
 }
-abline(v = time.plot[processStudent$changePoints], col = "red")
-abline(v = time.plot[processStudent$detectionTimes], col = "red", lty = 2)
-legend("topleft", legend = c("Estimated change point", "Detection time"), col = "red", lty = 1:2, bty = "n")
-dev.off()
+abline(v = year[processStudent$changePoints])
+abline(v = year[processStudent$detectionTimes], lty = 2)
+legend("topleft", legend = c("Estimated change point", "Detection time"), lty = 1:2, bty = "n")
