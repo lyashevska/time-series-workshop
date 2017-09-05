345969a473f78ea7be08d492823258dbda013b8e
Sean Anderson <sean@seananderson.ca>
2017-09-05 09:25:06 +0100

    Fix sim func

--- R/99-sim-gomp.R
+++ R/99-sim-gomp.R
@@ -3,8 +3,10 @@ sim_gomp <- function(nu = 10, sigma_proc = 0.65,
   lambda = 1.5, y1 = 3, seed = NULL) {
   
   y <- vector(length = N)
+  y[1] <- y1
   if (!is.null(seed)) set.seed(seed)
   proc_error <- metRology::rt.scaled(N, df = nu, mean = 0, sd = sigma_proc)
+  # proc_error <- rnorm(N, mean = 0, sd = sigma_proc)
   for(i in 2:N) {
     y[i] <- lambda + b * y[i-1] + proc_error[i-1]
   }
