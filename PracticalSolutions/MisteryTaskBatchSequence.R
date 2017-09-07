bcf1f160385da93e46efed0059ecd2e80e8bc8c0
Olga Lyashevska <olga@herenstraat.nl>
2017-09-07 11:26:52 +0100

    added batch sequance solution

---
+++ PracticalSolutions/MisteryTaskBatchSequence.R
@@ -0,0 +1,81 @@
+library(cpm)
+
+a<- read.csv("../data/a.csv")
+b<- read.csv("../data/b.csv")
+c<- read.csv("../data/c.csv")
+
+year <- a$year
+#year<-b$year
+#year<-c$year
+y <- a$index
+#y<-b$index
+#y<-c$index
+n <- length(y)
+
+
+## plot the data
+plot(year, y, type = "o", col = "purple",  pch = 19, bty = "l")
+
+##---------------------------------------
+## BATCH PROCESSING - SINGLE CHANGEPOINT 
+##---------------------------------------
+
+## Student t-test
+resultsStudent <- detectChangePointBatch(y, cpmType = "Student", alpha = 0.05)
+## check if change detected
+## plot out the test statistic
+plot(year, resultsStudent$Ds)
+abline(h = resultsStudent$threshold, lty = 2)
+points(year[resultsStudent$changePoint], resultsStudent$Ds[resultsStudent$changePoint], col = "red", cex = 2)
+
+## Mann-Whitney
+resultsMW <- detectChangePointBatch(y, cpmType = "Mann-Whitney", alpha = 0.05)
+## check if change detected
+## plot out the test statistic
+plot(year, resultsMW$Ds)
+abline(h = resultsMW$threshold, lty = 2)
+points(year[resultsMW$changePoint], resultsMW$Ds[resultsMW$changePoint], col = "red", cex = 2)
+
+## plot the data
+plot(year, y, type = "o", col = "purple", ylab = ylab, pch = 19, bty = "l", main = "Batch processing changepoints")
+abline(v = year[resultsStudent$changePoint], lty = 2, col = "magenta" )
+abline(v = year[resultsMW$changePoint], lty = 2, col = "forestgreen")
+legend("topright", legend = c("Student t", "Mann-Whitney"), lty = 2, col = c("magenta", "forestgreen"), bty = "n")
+
+##-----------------------------------------------------------
+## SEQUENTIAL PROCESSING - POTENTIALLY MULTIPLE CHANGEPOINTS 
+##-----------------------------------------------------------
+## Student t
+processStudent <- processStream(y, cpmType = "Student", startup = 10)
+## 
+
+k <- c(0, processStudent$changePoints, n)
+cols <- rainbow(length(processStudent$changePoints) + 1)
+
+plot(year, y, type = "o", col = "darkgrey",  pch = 19, bty = "l", main = "Sequential processing changepoints")
+for(i in 1:3){
+  idx <- (k[i] + 1):k[i + 1]
+  points(year[idx], y[idx], pch = 19, col = cols[i])
+  curve(mean(y[idx]) + 0 * x, from = year[k[i] + 1], to = year[k[i+1]], add = TRUE, col = cols[i])
+}
+abline(v = year[processStudent$changePoints])
+abline(v = year[processStudent$detectionTimes], lty = 2)
+legend("topleft", legend = c("Estimated change point", "Detection time"), lty = 1:2, bty = "n")
+
+## Mann Whit
+processStudent <- processStream(y, cpmType = "MW", startup = 10)
+## 
+
+k <- c(0, processStudent$changePoints, n)
+cols <- rainbow(length(processStudent$changePoints) + 1)
+
+plot(year2, y, type = "o", col = "darkgrey", ylab = ylab, pch = 19, bty = "l", main = "Sequential processing changepoints")
+for(i in 1:3){
+  idx <- (k[i] + 1):k[i + 1]
+  points(year[idx], y[idx], pch = 19, col = cols[i])
+  curve(mean(y[idx]) + 0 * x, from = year[k[i] + 1], to = year[k[i+1]], add = TRUE, col = cols[i])
+}
+abline(v = year[processStudent$changePoints])
+abline(v = year[processStudent$detectionTimes], lty = 2)
+legend("topleft", legend = c("Estimated change point", "Detection time"), lty = 1:2, bty = "n")
+
